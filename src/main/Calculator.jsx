74e4117fff528b6e5ed4915fff1d5b0e79305b68
denisbao <denis.tpd@gmail.com>
2022-03-25 10:09:52 -0300

    lógica da calculadora finalizada

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -8,7 +8,7 @@ import './Calculator.css'
 
 const initialState = {
   displayValue: '0',
-  cleadDisplay: false,
+  clearDisplay: false,
   operation: null,
   values: [0, 0],
   current: 0
@@ -27,12 +27,35 @@ export class Calculator extends Component {
     this.addDigit = this.addDigit.bind(this)
   }
 
+
   clearMemory() {
     this.setState({...initialState})
   }
 
-  setOperation() {
-    console.log('operacao')
+  setOperation(operation) {
+    // altera o indice do array quando uma operação for digitada:
+    if (this.state.current === 0) {
+      this.setState({operation, current: 1, clearDisplay: true})
+    }
+    else {
+      const equals = operation === '=' 
+      const currentOperation = this.state.operation
+      const values = [...this.state.values]
+      try {
+        values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+      } catch (e) {
+        values[0] = this.state.values[0]
+      }
+      values[1] = 0
+
+      this.setState({
+        displayValue: values[0], 
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values
+      })
+    }
   }
 
   addDigit(n) {
@@ -40,11 +63,11 @@ export class Calculator extends Component {
     if (n === '.' && this.state.displayValue.includes('.')) {
       return
     }
-    const clearDisplay = this.state.displayValue === '0' || this.state.cleadDisplay
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
     const currentValue = clearDisplay ? '' : this.state.displayValue
     const displayValue = currentValue + n
 
-    this.setState({displayValue, cleadDisplay: false})
+    this.setState({displayValue, clearDisplay: false})
 
     if (n !== '.') {
       const i = this.state.current
@@ -52,6 +75,7 @@ export class Calculator extends Component {
       const values = [...this.state.values]
       values[i] = newValue
       this.setState({values})
+      console.log(values, this.state.current)
     }
   }
 
