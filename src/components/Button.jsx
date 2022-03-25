619f5a00dac8dd419a5a8b622f0a03b03a5313e7
denisbao <denis.tpd@gmail.com>
2022-03-25 09:12:29 -0300

    layout geral e captura de valores dos botões

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,17 @@
 import './Button.css'
 
 export function Button(props) {
+
+  let styleClass = 'button '
+  styleClass += props.operation ? 'operation' : ''
+  styleClass += props.double ? 'double' : ''
+  styleClass += props.triple ? 'triple' : ''
+
   return (
-    <button className='button'>
+    <button 
+      className={styleClass}
+      onClick={e => props.click && props.click(props.label)}
+    >
       {props.label}
     </button>
   )
